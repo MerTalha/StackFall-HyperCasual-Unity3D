72e85365ddb78cc54989fa33f387b5e2a77806a3
Mert Talha Şimşek <merttalhasimsek@gmail.com>
2023-08-05 13:40:57 +0300

    Player Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Rigidbody rb;
+    bool carpa;
+
     void Start()
     {
+       rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            carpa = true;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            carpa = false;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(carpa)
+        {
+            rb.velocity = new Vector3(0,-100 * Time.fixedDeltaTime * 7 , 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!carpa)
+        {
+            rb.velocity = new Vector3(0, 50 * Time.deltaTime * 5, 0);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!carpa)
+        {
+            rb.velocity = new Vector3(0, 50 * Time.deltaTime * 5, 0);
+        }
     }
 }
